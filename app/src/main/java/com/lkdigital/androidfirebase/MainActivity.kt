38ab1a681f2458782736c7783acb93e1bf092512
King Ioane <thesamoanppprogrammer@gmail.com>
2022-01-27 13:10:17 +1300

    Added viewbinding to MainActivity.kt

--- app/src/main/java/com/lkdigital/androidfirebase/MainActivity.kt
+++ app/src/main/java/com/lkdigital/androidfirebase/MainActivity.kt
@@ -2,25 +2,37 @@ package com.lkdigital.androidfirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.lkdigital.androidfirebase.databinding.ActivityMainBinding
+import com.lkdigital.androidfirebase.databinding.FragmentTodoDetailBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        //setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         val navController = findNavController(R.id.nav_host_fragment)
         val appBarConfiguration = AppBarConfiguration(setOf(
             R.id.todosFragment
         ))
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        binding.navView.setupWithNavController(navController)
+
+        binding.fab.setOnClickListener {
+            navController.navigate(R.id.todoCreateFragment)
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
